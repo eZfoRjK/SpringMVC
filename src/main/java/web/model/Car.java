05e489414267f7126aebc7ffad95e82e045cb9e7
Alexander Lebedev <ezforjk@yandex.ru>
2024-08-26 11:13:05 +0300

    Осилил томкат

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,52 @@
+package web.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+    private String name;
+    private String model;
+    private int year;
+
+    public Car() {
+    }
+
+    public Car(String name, String model, int year) {
+        this.name = name;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
